e3e3f4a6a849c8b0643c32cd7b1d97ab2848a06c
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-05-30 20:42:48 +0530

    new

--- OOPs/DAY-1/All-in-one.cpp
+++ OOPs/DAY-1/All-in-one.cpp
@@ -1,71 +1,112 @@
-#include<iostream>
-// Not using namespace std;
+// #include<iostream>
+// // Not using namespace std;
 
-class All
+// class All
+// {
+// private:
+//     int n1, n2, sum;
+
+// public:
+//     void input()
+//     {
+//         std::cout << "Enter two numbers: ";
+//         std::cin >> n1 >> n2;
+//     }
+
+//     void addition()
+//     {
+//         sum = n1 + n2;
+//     }
+    
+//     void subtraction ()
+//     {
+//         sum = n1 - n2;
+//     }
+    
+//     void multiplication ()
+//     {
+//         sum = n1 * n2;
+//     }
+//     void division ()
+//     {
+//         sum = n1 / n2;
+//     }
+
+//     void output()
+//     {
+//         std::cout << "The sum is : " << sum <<std::endl;
+//     }
+    
+//         void output2()
+//     {
+//         std::cout << "The subtraction sum is : " << sum <<std::endl;
+//     }
+    
+//             void output3()
+//     {
+//         std::cout << "The multiply sum is : " << sum <<std::endl;
+//     }
+    
+//                 void output4()
+//     {
+//         std::cout << "The divide sum is : " << sum <<std::endl;
+//     }
+//     };
+
+//     int main()
+//     {
+//         All obj;
+//         obj.input();
+//         obj.addition();
+//         obj.output();
+//         obj.subtraction();
+//         obj.output2();
+//         obj.multiplication();
+//         obj.output3();
+//         obj.division();
+//         obj.output4();
+//      return 0;
+// }
+
+
+#include<iostream>
+using namespace std;
+
+class All_type 
 {
 private:
-    int n1, n2, sum;
+    int n1, n2, sum, difference, product, quotient;
 
 public:
-    void input()
+    void input() 
     {
-        std::cout << "Enter two numbers: ";
-        std::cin >> n1 >> n2;
+        cout << "Enter two numbers: ";
+        cin >> n1 >> n2;
     }
 
-    void addition()
+    void calculate() 
     {
         sum = n1 + n2;
-    }
-    
-    void subtraction ()
-    {
-        sum = n1 - n2;
-    }
-    
-    void multiplication ()
-    {
-        sum = n1 * n2;
-    }
-    void division ()
-    {
-        sum = n1 / n2;
+        difference = n1 - n2;
+        product = n1 * n2;
+        quotient = n1 / n2;
     }
 
-    void output()
+    void output() 
     {
-        std::cout << "The sum is : " << sum <<std::endl;
+        cout << "The addition is : " << sum << endl;
+        cout << "The subtraction is : " << difference << endl;
+        cout << "The multiplication is : " << product << endl;
+        cout << "The division is : " << quotient << endl;
     }
-    
-        void output2()
-    {
-        std::cout << "The subtraction sum is : " << sum <<std::endl;
-    }
-    
-            void output3()
-    {
-        std::cout << "The multiply sum is : " << sum <<std::endl;
-    }
-    
-                void output4()
-    {
-        std::cout << "The divide sum is : " << sum <<std::endl;
-    }
-    };
+};
 
-    int main()
-    {
-        All obj;
-        obj.input();
-        obj.addition();
-        obj.output();
-        obj.subtraction();
-        obj.output2();
-        obj.multiplication();
-        obj.output3();
-        obj.division();
-        obj.output4();
-        
+int main() 
+{
+    All_type obj;
+    obj.input();
+    obj.calculate();
+    obj.output();
 
-        return 0;
-    }
+    return 0;
+}
