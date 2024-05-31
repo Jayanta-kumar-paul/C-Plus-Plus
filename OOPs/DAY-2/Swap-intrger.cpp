f8d22a51cace2a761183adf2d0187916b95c12c3
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-05-31 23:45:56 +0530

    new

--- OOPs/DAY-2/Swap-intrger.cpp
+++ OOPs/DAY-2/Swap-intrger.cpp
@@ -1,4 +1,44 @@
-// WAP in C++ swap in two integers in class obj method.
+// // WAP in C++ swap in two integers in class obj method.
+// #include <iostream>
+
+// class IntegerSwap
+// {
+// private:
+//     int num1, num2;
+
+// public:
+//     void input()
+//     {
+//         std::cout << "Enter the two integers: ";
+//         std::cin >> num1 >> num2;
+//     }
+
+//     void swap()
+//     {
+//         int temp = num1;
+//         num1 = num2;
+//         num2 = temp;
+//     }
+
+//     void output()
+//     {
+//         std::cout << "Swapped integers: " << num1 << std::endl
+//                   << num2 << std::endl;
+//     }
+// };
+
+// int main()
+// {
+//     IntegerSwap obj;
+//     obj.input();
+//     obj.swap();
+//     obj.output();
+
+//     return 0;
+// }
+
+// simple C++ program that swaps two integers without using a third integer, using a class object:
+
 #include <iostream>
 
 class IntegerSwap
@@ -15,9 +55,9 @@ public:
 
     void swap()
     {
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
     }
 
     void output()
