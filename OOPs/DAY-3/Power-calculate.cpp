3346751863079508e6aebe2e88b08859d0413ed2
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-06-03 16:35:15 +0530

    modify

--- OOPs/DAY-3/Power-calculate.cpp
+++ OOPs/DAY-3/Power-calculate.cpp
@@ -1,43 +1,79 @@
-// c++ program that calculates the power of a number (x ^ y)  using class obj:
+// // c++ program that calculates the power of a number (x ^ y)  using class obj:
+
+// #include <iostream>
+// using namespace std;
+
+// class PowerCalculator
+// {
+// public:
+//     double x;
+//     int y;
+
+//     void input()
+//     {
+//         cout << "Enter base (x): ";
+//         cin >> x;
+//         cout << "Enter power (y): ";
+//         cin >> y;
+//     }
+
+//     double calculate()
+//     {
+//         double result = 1.0;
+//         for (int i = 0; i < y; i++)
+//         {
+//             result *= x;
+//         }
+//         return result;
+//     }
+
+//     void display()
+//     {
+//         cout <<  "The raised to the power of  (x^y) is: " << calculate() << endl;
+//     }
+// };
+
+// int main()
+// {
+//     PowerCalculator calculator;
+//     calculator.input();
+//     calculator.display();
+//     return 0;
+// }
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-class PowerCalculator 
+class PowerCalculator
 {
 public:
     double x;
     int y;
 
-    void input() 
+    void input()
     {
         cout << "Enter base (x): ";
         cin >> x;
         cout << "Enter power (y): ";
         cin >> y;
     }
-
-    double calculate() 
+    //using without return statement
+    void calculate()
     {
-        double result = 1.0;
-        for (int i = 0; i < y; i++) 
-        {
-            result *= x;
-        }
-        return result;
+        cout << "The raised to the power of (x^y) is: " << pow(x, y) << endl;
     }
 
-    void display() 
+    void display()
     {
-        cout <<  "The raised to the power of  (x^y) is: " << calculate() << endl;
+        calculate();
     }
 };
 
-int main() 
+int main()
 {
     PowerCalculator calculator;
     calculator.input();
     calculator.display();
     return 0;
 }
-
