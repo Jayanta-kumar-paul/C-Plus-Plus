c46b3b1189372676bae4793dbe34900e2388028e
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-08-05 23:36:42 +0530

    new

--- Inheritance/Singly Inheritance/Q1.cpp
+++ Inheritance/Singly Inheritance/Q1.cpp
@@ -1,61 +1,24 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-class A
-{
-public:
-    int roll;
-    void show()
-    {
-        cout << "The roll is " << roll << endl;
-    }
-
-private:
-    int marks;
-    void print()
-    {
-        cout << "The marks are " << marks << endl;
-    }
-
-protected:
-    int age;
-    void display()
-    {
-        cout << "The age is " << age << endl;
-    }
-};
-
-class B : public A
-{
-public:
-    string name;
-    void getdata()
-    {
-        cout << "My name is " << name << endl;
-    }
-};
-
-int main()
-{
-    A obj;
-    obj.roll = 10;
-    obj.show();
-    // obj.marks = 420; // Can't access private members
-    // obj.print();     // Can't access private members
-    // obj.age = 20;    // Can't access protected members
-    // obj.display();   // Can't access protected members
-
-    B obj1;
-    obj1.name = "Jayanta";
-    obj1.getdata();
-    obj1.roll = 15; // Can access public members inherited from class A
-    obj1.show();    // Can call public methods inherited from class A
-    // obj1.marks = 380; // Can't access private members
-    // obj1.print();     // Can't access private members
-    // obj1.age = 35;    // Can't access protected members
-    // obj1.display();   // Can't access protected members
-
-    return 0;
+// C++ program to explain Single inheritance 
+#include <iostream> 
+using namespace std; 
+// base class 
+class Vehicle  
+{ 
+public: 
+Vehicle() 
+{ 
+cout << "This is a Vehicle" << endl; 
+} 
+}; 
+// sub class derived from a single base classes 
+class Car: public Vehicle 
+{ 
+}; 
+// main function 
+int main() 
+{ 
+// creating object of sub class will 
+// invoke the constructor of base classes 
+Car obj; 
+return 0; 
 }
