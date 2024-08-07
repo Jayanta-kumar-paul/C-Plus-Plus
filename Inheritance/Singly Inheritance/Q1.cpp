2a257f22cda866e35a04e8ea6525218d5d0c864d
Jayanta-kumar-paul <kumarpaul912@gmail.com>
2024-08-07 13:28:46 +0530

    new

--- Inheritance/Singly Inheritance/Q1.cpp
+++ Inheritance/Singly Inheritance/Q1.cpp
@@ -1,24 +1,52 @@
-// C++ program to explain Single inheritance 
-#include <iostream> 
-using namespace std; 
-// base class 
-class Vehicle  
-{ 
-public: 
-Vehicle() 
-{ 
-cout << "This is a Vehicle" << endl; 
-} 
-}; 
-// sub class derived from a single base classes 
-class Car: public Vehicle 
-{ 
-}; 
-// main function 
-int main() 
-{ 
-// creating object of sub class will 
-// invoke the constructor of base classes 
-Car obj; 
-return 0; 
+#include <iostream>
+using namespace std;
+
+class base
+{
+public:
+    void sum(int a, int b)
+    {
+        int result;
+        result = a + b;
+        cout << "The sum is = " << result << endl;
+    }
+    void mul(int a, int b)
+    {
+        int result;
+        result = a * b;
+        cout << "The multiplication is = " << result << endl;
+    }
+};
+class derive : public base
+{
+public:
+    void sub(int a, int b)
+    {
+        int result;
+        result = a - b;
+        cout << "The subtraction is = " << result <<endl;
+    }
+    void div(int a, int b)
+    {
+        int result;
+        result = a / b;
+        cout << "The division is = " << result <<endl;
+    }
+};
+int main()
+{
+    int a, b;
+    cout << "enter two number :";
+    cin >> a >> b;
+
+    base obj1;
+    obj1.sum(a, b);
+    obj1.mul(a, b);
+
+    derive obj2;
+    obj2.sum(a, b);
+    obj2.mul(a, b);
+    obj2.sub(a, b);
+    obj2.div(a, b);
+    return 0;
 }
